2ec857e8ab0406fdfc93400ea1ae6d3709e51f77
hameedorefai <simpa.palestine@gmail.com>
2026-05-17 00:48:23 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using GenerativeAI.Microsoft;
+
+IChatClient client = 
+    new GenerativeAIChatClient(apiKey: "AIzaSyBESIweduXJzRBT8tk5If0nyf0TCw_Wpjo", "gemini-3.1-flash-lite");
+
+ChatClientAgent agent = new(client);
+
+
+
+while (true)
+{
+    Console.Write("User: ");
+    var userInput = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(userInput))
+        break;
+
+    var response = await agent.RunAsync(userInput);
+
+    Console.WriteLine("Agent: " + response.Text);
+}
